d0528941b5c83b6dd34eaa93498b2dd1528b131c
Ingolf Steinhardt <info@e-spin.de>
2018-03-26 09:20:36 +0200

    Hotfix move contents of database.sql to dca files

--- contao/dca/tl_metamodel_filtersetting.php
+++ contao/dca/tl_metamodel_filtersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_tags.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2018 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,7 +18,7 @@
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     David Molineus <mail@netzmacht.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2017 The MetaModels team.
+ * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_tags/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -40,12 +40,13 @@ $GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['tags extends d
 
 $GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['fields']['useor'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor'],
-    'exclude'                 => true,
-    'default'                 => true,
-    'inputType'               => 'checkbox',
-    'eval'                    => array
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor'],
+    'exclude'   => true,
+    'default'   => true,
+    'inputType' => 'checkbox',
+    'sql'       => 'char(1) NOT NULL default \'0\'',
+    'eval'      => array
     (
-        'tl_class'            => 'w50',
+        'tl_class' => 'w50',
     ),
 );
