165fb5e1cd439b9d64c767c5b177186f062525e6
Mini Model <minimodel@metamodel.me>
2022-11-19 18:10:50 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_filtersetting.php
+++ src/Resources/contao/languages/de/tl_metamodel_filtersetting.php
@@ -1,21 +1,33 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/filter_tags.
+ *
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2022-11-19T18:10:50+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2013-05-13T22:14:36+02:00
+ * @copyright 2012-2022 The MetaModels team.
+ * @license   https://github.com/MetaModels/filter_tags/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
 
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0'] = 'Nur verbleibende Tags';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1'] = 'Zeige nur Optionen, die zugeordnet sind, nachdem der Filter ausgeführt wurde.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['0']     = 'Nur zugeordnete Tags';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['1']     = 'Zeige nur Optionen, die im MetaModel bereits zugeordnet sind.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['tags'] = 'Mehrfachauswahl';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['0']        = 'ODER';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['1']        = 'ODER-Verknüpfung der Tags. Wird automatisch gesetzt, wenn das Attribut vom Typ "Auswahl" ist.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0']    = 'Nur verbleibende Tags';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1']    = 'Zeige nur Optionen, die zugeordnet sind, nachdem der Filter ausgeführt wurde.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['0']        = 'Nur zugeordnete Tags';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['1']        = 'Zeige nur Optionen, die im MetaModel bereits zugeordnet sind.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['show_select_all']['0'] = 'Checkbox "Alle hinzufügen" anzeigen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['show_select_all']['1'] = 'Wenn die Checkbox gesetzt ist, zeigen Sie das Kontrollkästchen "Alle hinzufügen" an, um alle Kontrollkästchen auf einmal zu aktivieren.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['tags']    = 'Mehrfachauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['0']           = 'ODER';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['1']           = 'ODER-Verknüpfung der Tags. Wird automatisch gesetzt, wenn das Attribut vom Typ "Auswahl" ist.';
+
